ffdc5f85e00756822168b7c6341e4750723b46af
Théophile <theophile.bourget.sio@gmail.com>
2022-10-20 14:34:59 +0200

    gitignore + modele.php

--- gsb-frais/src/Modele/modele.php
+++ gsb-frais/src/Modele/modele.php
@@ -8,7 +8,6 @@ require_once("ConnexionBDD.php");
 
 
 /*------------------------------VISITEUR------------------------------*/
-
 function getVisiteurAll() {
     try{
         $connexion = ConnexionBDD::getConnexion();
@@ -24,7 +23,6 @@ function getVisiteurAll() {
         return "BALISE ERREUR" . $e->getMessage();
     }
 }
-
 function loginVisiteur($login, $mdp){
     try{
         $connexion = ConnexionBDD::getConnexion();
@@ -37,15 +35,65 @@ function loginVisiteur($login, $mdp){
         ));
         $visiteurFetch = $visiteur->fetchAll();
         if(count($visiteurFetch) == 1 ){
-            return true;
+            return $visiteurFetch;
         }else{
-            return false;
+            return null;
         }
     }catch(PDOException $e){
-
+        echo $e->getMessage();
     }
 }
+function existsFicheFraisForVisiteurAndMois($idVisiteur, $mois){
+    try{
+        $connexion = ConnexionBDD::getConnexion();
+        $query = "SELECT * FROM FicheFrais "
+            ."WHERE idVisiteur = :visiteur "
+            ."AND mois = :mois ";
+        $ficheFrais = $connexion->prepare($query);
+        $ficheFrais->execute(array(
+            ":visiteur" => $idVisiteur,
+            ":mois" => $mois
+        ));
+        $ficheFraisFetch = $ficheFrais->fetchAll();
+        if(count($ficheFraisFetch) == 1){
+            return 1;
+        }elseif(count($ficheFraisFetch) > 1){
+            return -1;
+        }else{
+            return 0;
+        }
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+}
+function getLigneFraisForfaitAndFicheFraisForVisiteurAndMois($idVisiteur, $mois){
+    try{
+        $connexion = ConnexionBDD::getConnexion();
+        if(existsFicheFraisForVisiteurAndMois($idVisiteur, $mois) == 1){
+            $query = "SELECT * FROM LigneFraisForfait AS lff "
+                    ."JOIN FicheFrais AS ff "
+                    ."ON ff.idFicheFrais = lff.idFicheFrais "
+                    ."WHERE ff.mois = :mois AND ff.idVisiteur = :visiteur ";
+            $ligneFraisForfait = $connexion->prepare($query);
+            $ligneFraisForfait->execute(array(
+                ":visiteur" => $idVisiteur,
+                ":mois" => $mois
+            ));
+            $ligneFraisForfaitFetch = $ligneFraisForfait->fetchAll();
+            return $ligneFraisForfaitFetch;
+        }else{
 
+            /*---------------MEMO-----------------
+            $query = "INSERT INTO FicheFrais"
+            return null;
+            */
+            return null;
+        }
+
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+}
 
 /*------------------------------COMPTABLE------------------------------*/
 function loginComptable($login, $mdp){
@@ -65,10 +113,22 @@ function loginComptable($login, $mdp){
             return false;
         }
     }catch(PDOException $e){
-
+        echo $e->getMessage();
     }
 }
 
-
+/*------------------------------FRAISFORFAIT------------------------------*/
+function getFraisForfait(){
+    try{
+        $connexion = ConnexionBDD::getConnexion();
+        $query = "SELECT * FROM FraisForfait";
+        $fraisForfait = $connexion->prepare($query);
+        $fraisForfait->execute();
+        $fraisForfaitFetch = $fraisForfait->fetchAll();
+        return $fraisForfaitFetch;
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+}
 
 ?>
