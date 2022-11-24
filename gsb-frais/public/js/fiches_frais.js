2f16eb42d380e9b3614fd8ca700f5fe9ff98ce7e
Zack-src <gdesramaux@yahoo.fr>
2022-11-24 17:52:11 +0100

    remove unused js func

--- gsb-frais/public/js/fiches_frais.js
+++ gsb-frais/public/js/fiches_frais.js
@@ -1,12 +1,3 @@
-function setClass(el, className, fn){
-    if(el) {
-      for (var i of el) {
-        if(typeof className === "string")
-           i.setAttribute("class", className);
-        if(fn) fn(i);
-      }
-    }
-  }
 
 function add_frais()
 {
@@ -34,12 +25,13 @@ function add_frais()
         textboxCell.innerHTML = '<input name="frais_hors_forfait[' + nb_input + '][libelle]" id="td_libelle_' + nb_input + '" class="input_libelle" type="text" value="">';
         dateCell.innerHTML = '<input name="frais_hors_forfait[' + nb_input +'][date]" id="td_date_' + nb_input + '" class="input_date" type="date">';
         inputCell.innerHTML = '<input name="frais_hors_forfait[' + nb_input +'][input]" id="td_input_' + nb_input + '" class="input_prix" type="number" value="0" min="0">';
-        removeCell.innerHTML = '<i class="fa-solid fa-trash" onclick="remove_frais(' + nb_input + ')">';
+        removeCell.innerHTML = '<i class="fa-solid fa-trash" onclick="remove_frais(this)">';
     }
 }
 
 function remove_frais(button)
 {
   let table = document.getElementById("table_hors_frais");
+
   table.deleteRow(button.parentElement.parentElement.rowIndex);
 }
