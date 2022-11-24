37576f4183b9b1f7daa2d86e93a2a533edfdc85d
Zack-src <gdesramaux@yahoo.fr>
2022-11-24 16:52:02 +0100

    fix : visiteur fiche frais HF js (add, remove)

--- gsb-frais/public/js/fiches_frais.js
+++ gsb-frais/public/js/fiches_frais.js
@@ -13,8 +13,10 @@ function add_frais()
     let table = document.getElementById("table_hors_frais");
     nb_input = table.rows.length-2;
 
-    var current_input = 'td_libelle_'+nb_input;
-    var input_value = document.getElementById(current_input).value
+    var row = table.getElementsByTagName("tr")[table.rows.length - 1];
+    colum = row.getElementsByTagName("td")[0];
+
+    input_value = colum.getElementsByTagName("input")[0].value;
 
     if(input_value != "")
     {
@@ -25,12 +27,19 @@ function add_frais()
         let textboxCell = newRow.insertCell(0);
         let dateCell = newRow.insertCell(1);
         let inputCell = newRow.insertCell(2);
+        let removeCell = newRow.insertCell(3);
 
         dateCell.className = "td_date";
 
         textboxCell.innerHTML = '<input name="frais_hors_forfait[' + nb_input + '][libelle]" id="td_libelle_' + nb_input + '" class="input_libelle" type="text" value="">';
         dateCell.innerHTML = '<input name="frais_hors_forfait[' + nb_input +'][date]" id="td_date_' + nb_input + '" class="input_date" type="date">';
         inputCell.innerHTML = '<input name="frais_hors_forfait[' + nb_input +'][input]" id="td_input_' + nb_input + '" class="input_prix" type="number" value="0" min="0">';
-
+        removeCell.innerHTML = '<i class="fa-solid fa-trash" onclick="remove_frais(' + nb_input + ')">';
     }
+}
+
+function remove_frais(button)
+{
+  let table = document.getElementById("table_hors_frais");
+  table.deleteRow(button.parentElement.parentElement.rowIndex);
 }
